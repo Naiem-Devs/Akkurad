0ec19ae688ee7381902d887375c64441152d1dea
Naiem-Devs <dnsnaiem@gmail.com>
2023-01-18 17:22:22 +0600

    p done

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+(function($) {
+  "use strict";
+  
+ // menu 
+  $('.siteBar-btn').click( function (){ 
+    $('.mobile-menu').toggleClass('siteBar');   
+  }); 
+
+
+  // owlCarousel
+  $(".feedback_slider").owlCarousel({
+    loop: false,
+    margin: 30,
+    items: 1,
+    navText: [
+      '<i class="far fa-chevron-left"></i>',
+      '<i class="far fa-chevron-right"></i>'
+    ],
+    nav: true,
+    dots: false
+  });
+
+ 
+})(jQuery);
